ef097b585d414d744b55d11bb40915f268225f0d
Brendon stead <97539241+b-stead@users.noreply.github.com>
2022-12-26 15:27:24 +0200

    Update settings.py

--- demosite/settings.py
+++ demosite/settings.py
@@ -23,7 +23,9 @@ APP_NAME = 'demosite'
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+#SECRET_KEY = config("SECRET_KEY")
+#Heroku deployment
+SECRET_KEY = os.environ[‘secret_key’]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
